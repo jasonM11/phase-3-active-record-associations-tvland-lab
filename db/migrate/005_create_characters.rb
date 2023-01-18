e8787e3d45e2030f46027f3b32714a44d5180dbe
Jason Mejia <Jasonsolano63@icloud.com>
2023-01-18 17:03:12 -0500

    commit tree 0500eeadb8a4d0d91c1b5f4a229e057755eb91fb

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,12 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
+    create_table :characters do |t|
+      t.string :last_name
+      t.interger :actor_id
+      t.integer :show_id
+      t.string :catchphrase
+    end 
+
     # Your code here
   end
 end
